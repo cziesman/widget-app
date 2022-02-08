58c85cd006ce21151a8062817f98cff4414cb67e
Craig Ziesman <craig.ziesman@gmail.com>
2022-02-08 12:55:30 -0600

    Update OpenApiConfig.java

--- src/main/java/com/redhat/scale/api/OpenApiConfig.java
+++ src/main/java/com/redhat/scale/api/OpenApiConfig.java
@@ -11,6 +11,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI initOpenAPI() {
 
-        return new OpenAPI().info(new Info().title("Widget API").description("The Widget Backend Service").version("1.0"));
+        return new OpenAPI().info(new Info().title("Widget API").description("The Widget Service").version("1.0"));
     }
 }
